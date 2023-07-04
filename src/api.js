2ec3e453302ea9fdf175a5e737777b9e105ad8bc
hostdog01 <hostdog01@naver.com>
2023-07-04 09:58:57 +0000

    ff

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL:
-    "/api", // "http://localhost:8080/",
+    "./api", // "http://localhost:8080/",
   timeout: 1000,
   headers: {
     "Content-Type": "application/json",
