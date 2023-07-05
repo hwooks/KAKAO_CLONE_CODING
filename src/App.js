2dccb265c8ff35e907ef233733a30156ab4f6e27
hostdog01 <hostdog01@naver.com>
2023-07-05 02:23:10 +0000

    dd

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ import NotFound from './routes/NotFound'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const staticServerUri = process.env.REACT_APP_PATH;
+const staticServerUri = "";
 
 function App() {
 
