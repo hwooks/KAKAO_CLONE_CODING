7d9bc0ca4922b9f8d9ab7db98a22e14ea22c2437
eden.kim <eden.kim@goorm.io>
2023-07-25 09:04:41 +0000

    dotenv config deleted

--- startServer.sh
+++ startServer.sh
@@ -2,4 +2,4 @@ rm /etc/nginx/sites-enabled/default
 service nginx start
 service mysql restart
 (cd /goormService/backend && gradle wrapper && ./gradlew clean build )
-java -jar -Dspring.profiles.active=local /goormService/backend/build/libs/kakao-1.0.jar
+java -jar -Dspring.profiles.active=ide /goormService/backend/build/libs/kakao-1.0.jar
